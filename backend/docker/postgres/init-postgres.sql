692dbd739931ead9a3060204112f3fc90bb2eeb8
BLockhartCC <blockhart@cryptocompare.com>
2021-05-28 21:21:14 +0100

    Build DB in Postgres via Docker

---
+++ backend/docker/postgres/init-postgres.sql
@@ -0,0 +1,22 @@
+CREATE TABLE venues (
+    name varchar(50) NOT NULL,
+    category varchar(50) NOT NULL,
+    url varchar(300) NOT NULL,
+    date TIMESTAMP NOT NULL,
+    excerpt text,
+    thumbnail varchar(300) NOT NULL,
+    lat double PRECISION,
+    lng double PRECISION,
+    address varchar(200) NOT NULL,
+    phone varchar(20) NOT NULL,
+    twitter varchar(20) NOT NULL,
+    stars_beer double PRECISION,
+    stars_atmosphere double PRECISION,
+    stars_amenities double PRECISION,
+    stars_value double PRECISION,
+    tags text
+);
+
+CREATE INDEX idx_name ON venues (name DESC);
+
+COPY venues(name, category, url, date, excerpt, thumbnail, lat, lng, address, phone, twitter, stars_beer, stars_atmosphere, stars_amenities, stars_value, tags) FROM '/docker-entrypoint-initdb.d/leedsbeerquest.csv' DELIMITER ',' CSV HEADER;
